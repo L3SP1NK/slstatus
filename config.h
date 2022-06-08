23a2dae4ef224631b803cc45e48e50c467bbaf78
root <elespink@gmail.com>
2022-06-08 21:39:03 +0200

    Minor changes.

--- config.h
+++ config.h
@@ -63,8 +63,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-//	{ cpu_perc, "[CPU  %s%%]   ", NULL },
-//	{ ram_perc, "[RAM  %s%%]   ", NULL },
-//	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ cpu_perc, "[CPU  %s%%] ", NULL },
+	{ ram_perc, "[RAM  %s%%] | ", NULL },
+	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ datetime, "%s",           " %a %d %b, %H:%M " },
 };
